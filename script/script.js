d06b6927a3ae8992f420db16f181eacc154af0cc
Vic <apulivic@gmail.com>
2022-08-13 17:28:13 +0800

    shortened script

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const gameContainer = document.querySelector(".tic-box");
 const boxes = document.querySelectorAll(".box");
+const gameContainer = document.querySelector(".board");
 
 // HEADERS
 const playerTurnEl = document.querySelector(".player-toggle");
@@ -16,7 +16,7 @@ const replayEl = document.querySelector(".replay");
 const nextEl = document.querySelector(".replay-next");
 const previousEl = document.querySelector(".replay-previous");
 
-let gameOver = false;
+let currentPlayer = ["X", "O"];
 
 // FIND WAY TO REMOVE/SHORTEN
 const row1col1 = document.querySelector(".row-one-col-one");
@@ -29,34 +29,40 @@ const row3col1 = document.querySelector(".row-three-col-one");
 const row3col2 = document.querySelector(".row-three-col-two");
 const row3col3 = document.querySelector(".row-three-col-three");
 
-// USED TO CHECK IF BOXES/TILES HAVE VALUES AND DISABLE CHANGE PLAYER
-// STUDY THIS
+// USED TO CHECK IF BOXES/TILES HAVE VALUES AND DISABLE CHANGE PLAYER BUTTON THEN CHECKS FOR WINNER EACH MOVE
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (boxes.textContent === "X" || "O") {
       changeEl.style.opacity = "0";
       changeEl.style.transform = "translateY(-20rem)";
+      if (playerTurnEl.textContent === "X" && box.textContent === "") {
+        box.textContent = "X";
+        playerTurnEl.textContent = "O";
+        box.style.cursor = "not-allowed";
+      } else if (playerTurnEl.textContent === "O" && box.textContent === "") {
+        box.textContent = "O";
+        playerTurnEl.textContent = "X";
+        box.style.cursor = "not-allowed";
+      } else {
+        console.log("Tile already taken");
+      }
+      checkWinner();
     }
   });
 });
 
-// MAKE RESTART BUTTON WORK
-
 // DECLARES WINNER AND DISABLES BOARD
 const winnerWinner = () => {
   if (playerTurnEl.textContent === "X") {
-    playerTurnEl.textContent === "O";
-  } else {
-    playerTurnEl.textContent === "X";
+    winner.textContent = currentPlayer[1];
+  } else if (playerTurnEl.textContent === "O") {
+    winner.textContent = currentPlayer[0];
   }
-  winner.textContent = playerTurnEl.textContent;
-  console.log(`Player ${playerTurnEl.textContent} is the winner`);
   restartEl.style.opacity = "1";
   winnerEl.style.fontSize = "6rem";
   winnerEl.style.opacity = "1";
   replayEl.style.opacity = "1";
   gameContainer.style.pointerEvents = "none";
-  gameOver = true;
 };
 
 // CHANGE PLAYER TURN
@@ -145,138 +151,153 @@ const checkWinner = () => {
   }
 };
 
+// const checkWinner = () => {
+//   if ((boxes[0] && boxes[1] && boxes[2] === "X") || "O") {
+//     winnerWinner();
+//   }
+// };
+//   ) {
+//     winnerWinner();
+//   }
+// let arr = [
+//   ["", "", ""],
+//   ["", "", ""],
+//   ["", "", ""],
+// ];
+
+// if (boxes[0], boxes[1], boxes[2] === )
 // FIND WAY TO SHORTEN
-row1col1.addEventListener("click", () => {
-  if (playerTurnEl.textContent === "X" && row1col1.textContent === "") {
-    row1col1.textContent = "X";
-    playerTurnEl.textContent = "O";
-    row1col1.style.cursor = "not-allowed";
-  } else if (playerTurnEl.textContent === "O" && row1col1.textContent === "") {
-    row1col1.textContent = "O";
-    playerTurnEl.textContent = "X";
-    row1col1.style.cursor = "not-allowed";
-  } else {
-    console.log("Tile already taken");
-  }
-  checkWinner();
-});
+// row1col1.addEventListener("click", () => {
+//   if (playerTurnEl.textContent === "X" && row1col1.textContent === "") {
+//     row1col1.textContent = "X";
+//     playerTurnEl.textContent = "O";
+//     row1col1.style.cursor = "not-allowed";
+//   } else if (playerTurnEl.textContent === "O" && row1col1.textContent === "") {
+//     row1col1.textContent = "O";
+//     playerTurnEl.textContent = "X";
+//     row1col1.style.cursor = "not-allowed";
+//   } else {
+//     console.log("Tile already taken");
+//   }
+//   checkWinner();
+// });
 
-row1col2.addEventListener("click", () => {
-  if (playerTurnEl.textContent === "X" && row1col2.textContent === "") {
-    row1col2.textContent = "X";
-    playerTurnEl.textContent = "O";
-    row1col2.style.cursor = "not-allowed";
-  } else if (playerTurnEl.textContent === "O" && row1col2.textContent === "") {
-    row1col2.textContent = "O";
-    playerTurnEl.textContent = "X";
-    row1col2.style.cursor = "not-allowed";
-  } else {
-    console.log("Tile already taken");
-  }
-  checkWinner();
-});
+// row1col2.addEventListener("click", () => {
+//   if (playerTurnEl.textContent === "X" && row1col2.textContent === "") {
+//     row1col2.textContent = "X";
+//     playerTurnEl.textContent = "O";
+//     row1col2.style.cursor = "not-allowed";
+//   } else if (playerTurnEl.textContent === "O" && row1col2.textContent === "") {
+//     row1col2.textContent = "O";
+//     playerTurnEl.textContent = "X";
+//     row1col2.style.cursor = "not-allowed";
+//   } else {
+//     console.log("Tile already taken");
+//   }
+//   checkWinner();
+// });
 
-row1col3.addEventListener("click", () => {
-  if (playerTurnEl.textContent === "X" && row1col3.textContent === "") {
-    row1col3.textContent = "X";
-    playerTurnEl.textContent = "O";
-    row1col3.style.cursor = "not-allowed";
-  } else if (playerTurnEl.textContent === "O" && row1col3.textContent === "") {
-    row1col3.textContent = "O";
-    playerTurnEl.textContent = "X";
-    row1col3.style.cursor = "not-allowed";
-  } else {
-    console.log("Tile already taken");
-  }
-  checkWinner();
-});
+// row1col3.addEventListener("click", () => {
+//   if (playerTurnEl.textContent === "X" && row1col3.textContent === "") {
+//     row1col3.textContent = "X";
+//     playerTurnEl.textContent = "O";
+//     row1col3.style.cursor = "not-allowed";
+//   } else if (playerTurnEl.textContent === "O" && row1col3.textContent === "") {
+//     row1col3.textContent = "O";
+//     playerTurnEl.textContent = "X";
+//     row1col3.style.cursor = "not-allowed";
+//   } else {
+//     console.log("Tile already taken");
+//   }
+//   checkWinner();
+// });
 
-row2col1.addEventListener("click", () => {
-  if (playerTurnEl.textContent === "X" && row2col1.textContent === "") {
-    row2col1.textContent = "X";
-    playerTurnEl.textContent = "O";
-    row2col1.style.cursor = "not-allowed";
-  } else if (playerTurnEl.textContent === "O" && row2col1.textContent === "") {
-    row2col1.textContent = "O";
-    playerTurnEl.textContent = "X";
-    row2col1.style.cursor = "not-allowed";
-  } else {
-    console.log("Tile already taken");
-  }
-  checkWinner();
-});
+// row2col1.addEventListener("click", () => {
+//   if (playerTurnEl.textContent === "X" && row2col1.textContent === "") {
+//     row2col1.textContent = "X";
+//     playerTurnEl.textContent = "O";
+//     row2col1.style.cursor = "not-allowed";
+//   } else if (playerTurnEl.textContent === "O" && row2col1.textContent === "") {
+//     row2col1.textContent = "O";
+//     playerTurnEl.textContent = "X";
+//     row2col1.style.cursor = "not-allowed";
+//   } else {
+//     console.log("Tile already taken");
+//   }
+//   checkWinner();
+// });
 
-row2col2.addEventListener("click", () => {
-  if (playerTurnEl.textContent === "X" && row2col2.textContent === "") {
-    row2col2.textContent = "X";
-    playerTurnEl.textContent = "O";
-    row2col2.style.cursor = "not-allowed";
-  } else if (playerTurnEl.textContent === "O" && row2col2.textContent === "") {
-    row2col2.textContent = "O";
-    playerTurnEl.textContent = "X";
-    row2col2.style.cursor = "not-allowed";
-  } else {
-    console.log("Tile already taken");
-  }
-  checkWinner();
-});
+// row2col2.addEventListener("click", () => {
+//   if (playerTurnEl.textContent === "X" && row2col2.textContent === "") {
+//     row2col2.textContent = "X";
+//     playerTurnEl.textContent = "O";
+//     row2col2.style.cursor = "not-allowed";
+//   } else if (playerTurnEl.textContent === "O" && row2col2.textContent === "") {
+//     row2col2.textContent = "O";
+//     playerTurnEl.textContent = "X";
+//     row2col2.style.cursor = "not-allowed";
+//   } else {
+//     console.log("Tile already taken");
+//   }
+//   checkWinner();
+// });
 
-row2col3.addEventListener("click", () => {
-  if (playerTurnEl.textContent === "X" && row2col3.textContent === "") {
-    row2col3.textContent = "X";
-    playerTurnEl.textContent = "O";
-    row2col3.style.cursor = "not-allowed";
-  } else if (playerTurnEl.textContent === "O" && row2col3.textContent === "") {
-    row2col3.textContent = "O";
-    playerTurnEl.textContent = "X";
-    row2col3.style.cursor = "not-allowed";
-  } else {
-    console.log("Tile already taken");
-  }
-  checkWinner();
-});
+// row2col3.addEventListener("click", () => {
+//   if (playerTurnEl.textContent === "X" && row2col3.textContent === "") {
+//     row2col3.textContent = "X";
+//     playerTurnEl.textContent = "O";
+//     row2col3.style.cursor = "not-allowed";
+//   } else if (playerTurnEl.textContent === "O" && row2col3.textContent === "") {
+//     row2col3.textContent = "O";
+//     playerTurnEl.textContent = "X";
+//     row2col3.style.cursor = "not-allowed";
+//   } else {
+//     console.log("Tile already taken");
+//   }
+//   checkWinner();
+// });
 
-row3col1.addEventListener("click", () => {
-  if (playerTurnEl.textContent === "X" && row3col1.textContent === "") {
-    row3col1.textContent = "X";
-    playerTurnEl.textContent = "O";
-    row3col1.style.cursor = "not-allowed";
-  } else if (playerTurnEl.textContent === "O" && row3col1.textContent === "") {
-    row3col1.textContent = "O";
-    playerTurnEl.textContent = "X";
-    row3col1.style.cursor = "not-allowed";
-  } else {
-    console.log("Tile already taken");
-  }
-  checkWinner();
-});
+// row3col1.addEventListener("click", () => {
+//   if (playerTurnEl.textContent === "X" && row3col1.textContent === "") {
+//     row3col1.textContent = "X";
+//     playerTurnEl.textContent = "O";
+//     row3col1.style.cursor = "not-allowed";
+//   } else if (playerTurnEl.textContent === "O" && row3col1.textContent === "") {
+//     row3col1.textContent = "O";
+//     playerTurnEl.textContent = "X";
+//     row3col1.style.cursor = "not-allowed";
+//   } else {
+//     console.log("Tile already taken");
+//   }
+//   checkWinner();
+// });
 
-row3col2.addEventListener("click", () => {
-  if (playerTurnEl.textContent === "X" && row3col2.textContent === "") {
-    row3col2.textContent = "X";
-    playerTurnEl.textContent = "O";
-    row3col2.style.cursor = "not-allowed";
-  } else if (playerTurnEl.textContent === "O" && row3col2.textContent === "") {
-    row3col2.textContent = "O";
-    playerTurnEl.textContent = "X";
-    row3col2.style.cursor = "not-allowed";
-  } else {
-    console.log("Tile already taken");
-  }
-  checkWinner();
-});
+// row3col2.addEventListener("click", () => {
+//   if (playerTurnEl.textContent === "X" && row3col2.textContent === "") {
+//     row3col2.textContent = "X";
+//     playerTurnEl.textContent = "O";
+//     row3col2.style.cursor = "not-allowed";
+//   } else if (playerTurnEl.textContent === "O" && row3col2.textContent === "") {
+//     row3col2.textContent = "O";
+//     playerTurnEl.textContent = "X";
+//     row3col2.style.cursor = "not-allowed";
+//   } else {
+//     console.log("Tile already taken");
+//   }
+//   checkWinner();
+// });
 
-row3col3.addEventListener("click", () => {
-  if (playerTurnEl.textContent === "X" && row3col3.textContent === "") {
-    row3col3.textContent = "X";
-    playerTurnEl.textContent = "O";
-    row3col3.style.cursor = "not-allowed";
-  } else if (playerTurnEl.textContent === "O" && row3col3.textContent === "") {
-    row3col3.textContent = "O";
-    playerTurnEl.textContent = "X";
-    row3col3.style.cursor = "not-allowed";
-  } else {
-    console.log("Tile already taken");
-  }
-  checkWinner();
-});
+// row3col3.addEventListener("click", () => {
+//   if (playerTurnEl.textContent === "X" && row3col3.textContent === "") {
+//     row3col3.textContent = "X";
+//     playerTurnEl.textContent = "O";
+//     row3col3.style.cursor = "not-allowed";
+//   } else if (playerTurnEl.textContent === "O" && row3col3.textContent === "") {
+//     row3col3.textContent = "O";
+//     playerTurnEl.textContent = "X";
+//     row3col3.style.cursor = "not-allowed";
+//   } else {
+//     console.log("Tile already taken");
+//   }
+//   checkWinner();
+// });
